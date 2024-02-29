2387e615be3efce67352e1d97c1321460e175858
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2024-02-29 20:20:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let ele=document.createElement("h1");
+let div=document.getElementById("sizeInfo");
+ele.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+div.append(ele);
+
+window.onresize=function(){
+	ele.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+};

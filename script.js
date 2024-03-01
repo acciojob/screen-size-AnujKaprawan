cad981d00ad074aadd0028451d31ac63531596f1
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2024-03-01 20:29:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 //your JS code here. If required.
-let ele=document.createElement("h1");
-let div=document.getElementById("sizeInfo");
-ele.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
-div.append(ele);
+// script.js
 
-window.onresize=function(){
-	ele.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`
-};
+// Function to update size information
+function updateSizeInfo() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const sizeInfo = document.getElementById('sizeInfo');
+    sizeInfo.innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
+}
+
+// Initial call to update size information
+updateSizeInfo();
+
+// Event listener for window resize
+window.addEventListener('resize', updateSizeInfo);
